914b6a8576253db1ab38ce14949c840adfbb2a0b
AleksandarSavic95 <alek_bn@hotmail.com>
2018-11-13 16:43:33 +0100

    #ASLR-27 basic jwt setup (tested with Postman)

--- app/User.php
+++ app/User.php
@@ -2,11 +2,11 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -34,5 +34,27 @@ class User extends Authenticatable
     public function todoItems()
     {
         return $this->hasMany(TodoItem::class);
+    }
+
+    // JWT support
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
